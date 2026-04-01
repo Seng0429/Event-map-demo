dd5e7395a788e32fb86f3713c80c0190f962cdf4
Theng Wei Seng <t.w.seng96@gmail.com>
2026-04-02 02:10:57 +0800

    Revert

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import GalleryCorridor from './components/GalleryCorridor/GalleryCorridor';
 import ClaudePhotoGallery from './components/ClaudePhotoGallery/ClaudePhotoGallery';
 import Timeline from './components/Timeline/Timeline';
 
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter basename='/Event-map-demo'>
+      <BrowserRouter basename='/Event-map-demo'>
         <Routes>
           <Route path="/" element={<ClaudePhotoGallery />} />
           <Route path="demo1" element={<ClaudePhotoGallery />} />
           <Route path="demo2" element={<GalleryCorridor />} />
           <Route path="demo3" element={<Timeline />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
